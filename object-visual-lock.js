35de71768a22dd5e18296b860d38231c13d9a2f5
Trevor Larson <trevorllarson@gmail.com>
2017-10-01 20:08:09 -0500

    Adds all 4 corner directions and makes lock button bigger

--- object-visual-lock.js
+++ object-visual-lock.js
@@ -7,17 +7,22 @@
 
         var controlPageObject = '<!-- Object Visual Lock -->' +
         '<script src="https://use.fontawesome.com/b47b7f257b.js"></script>' +
-        // '<div class="lock-window-toggle-button" style="postition: fixed; top: 10px; right: 10px; height: 30px; width: 30px; z-index: 100000000; background: white;">' +
-        //     '' +
-        // '</div>'
-        '<div class="lock-window-object" style="position: fixed; top: 10px; right: 10px; height: 20px; width: 60px; z-index: 100000000; background: white;">' +
-            '<div class="lock-window-set-to-top" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; color: yellow; font-size: 12px;">' +
-                '<i class="fa fa-arrow-up" aria-hidden="true"></i>' +
-            '</div>' + 
-            '<div class="lock-window-set-to-bottom" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: black;">' +
-                '<i class="fa fa-arrow-down" aria-hidden="true"></i>' +
+        '<div class="lock-window-object" style="position: fixed; top: 10px; right: 10px; height: 40px; width: 60px; z-index: 100000000; background: white;">' +
+            '<div style="width: 40px; height: 40px; float: left;">' +
+                '<div class="lock-window-set-to-top-left lock-window-position-button" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: black;">' +
+                    '<i style="transform: rotate(-45deg);" class="fa fa-arrow-up" aria-hidden="true"></i>' +
+                '</div>' +
+                '<div class="lock-window-set-to-top-right lock-window-position-button" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: yellow;">' +
+                    '<i style="transform: rotate(45deg);" class="fa fa-arrow-up" aria-hidden="true"></i>' +
+                '</div>' +
+                '<div class="lock-window-set-to-bottom-left lock-window-position-button" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: black;">' +
+                    '<i style="transform: rotate(45deg);" class="fa fa-arrow-down" aria-hidden="true"></i>' +
+                '</div>' +
+                '<div class="lock-window-set-to-bottom-right lock-window-position-button" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: black;">' +
+                    '<i style="transform: rotate(-45deg);" class="fa fa-arrow-down" aria-hidden="true"></i>' +
+                '</div>' +
             '</div>' +
-            '<div class="lock-window-on-off" style="height: 20px; width: 20px; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 0px; font-size: 12px; color: black;">' +
+            '<div class="lock-window-on-off" style="height: 40px; width: 20px; float: left; text-align: center; background: grey; border: 1px solid black; float: left; cursor: pointer; text-align: center; padding-top: 10px; font-size: 12px; color: black;">' +
                 '<i class="fa fa-power-off" aria-hidden="true"></i>' +
             '</div>' + 
         '</div>';
@@ -26,8 +31,10 @@
 
         var lockWindowObject = $('.lock-window-object'),
             lockWindowOnOff = $('.lock-window-object .lock-window-on-off'),
-            lockWindowToTop = $('.lock-window-object .lock-window-set-to-top'),
-            lockWindowToBottom = $('.lock-window-object .lock-window-set-to-bottom');
+            lockWindowToTopLeft = $('.lock-window-object .lock-window-set-to-top-left'),
+            lockWindowToTopRight = $('.lock-window-object .lock-window-set-to-top-right'),
+            lockWindowToBottomLeft = $('.lock-window-object .lock-window-set-to-bottom-left');
+            lockWindowToBottomRight = $('.lock-window-object .lock-window-set-to-bottom-right');
 
         // Set up up and down control buttons and on and off. and then get the scroll lock
         lockWindowOnOff.on('click', function() {
@@ -40,16 +47,28 @@
             }
         });
 
-        lockWindowToBottom.on('click', function() {
-            lockWindowObject.css({ 'top': 'auto', 'bottom': '10px' });
-            lockWindowToBottom.css('color', 'yellow');
-            lockWindowToTop.css('color', 'black');
+        lockWindowToTopLeft.on('click', function() {
+            lockWindowObject.css({ 'top': '10px', 'bottom': 'auto', 'left': '10px', 'right': 'auto' });
+            $('.lock-window-position-button').not(lockWindowToTopLeft).css('color', 'black');
+            lockWindowToTopLeft.css('color', 'yellow');
         });
 
-        lockWindowToTop.on('click', function() {
-            lockWindowObject.css({ 'top': '10px', 'bottom': 'auto' });
-            lockWindowToTop.css('color', 'yellow');
-            lockWindowToBottom.css('color', 'black');
+        lockWindowToTopRight.on('click', function() {
+            lockWindowObject.css({ 'top': '10px', 'bottom': 'auto', 'left': 'auto', 'right': '10px' });
+            $('.lock-window-position-button').not(lockWindowToTopRight).css('color', 'black');
+            lockWindowToTopRight.css('color', 'yellow');
+        });
+
+        lockWindowToBottomLeft.on('click', function() {
+            lockWindowObject.css({ 'top': 'auto', 'bottom': '10px', 'left': '10px', 'right': 'auto' });
+            $('.lock-window-position-button').not(lockWindowToBottomLeft).css('color', 'black');
+            lockWindowToBottomLeft.css('color', 'yellow');
+        });
+
+        lockWindowToBottomRight.on('click', function() {
+            lockWindowObject.css({ 'top': 'auto', 'bottom': '10px', 'left': 'auto', 'right': '10px' });
+            $('.lock-window-position-button').not(lockWindowToBottomRight).css('color', 'black');
+            lockWindowToBottomRight.css('color', 'yellow');
         });
 
         $(window).on('resize', function() {
